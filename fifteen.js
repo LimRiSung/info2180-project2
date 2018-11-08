5f7788bfae5bd3bceb82734347f6844bc0d94b3a
Chrissie <32311634+chrissiewilliams@users.noreply.github.com>
2018-11-07 20:17:58 -0500

    Added image choices and functionality to select background

--- fifteen.js
+++ fifteen.js
@@ -1,4 +1,4 @@
-/*Apperance Details*/
+/*Appearance Details*/
 
 //var puzzlepiecesstyles;
 window.onload = addPuzzlePiecesStyles;
@@ -14,5 +14,57 @@ function addPuzzlePiecesStyles() {
         puzzlepieces[i].style.position = "relative";
         puzzlepieces[i].style.float = "left";
     } 
-                                                                                                                                             
+  //tileBackground();   
+  additionalFeature();                                                                                                                                
 }
+
+/*function tileBackground()
+{
+    var puzzlepieces, bgWidth, bgHeight;
+    puzzlepieces = document.getElementById('puzzlearea').getElementsByTagName('div'); 
+    bgWidth = 400;
+    bgHeight = 400;
+    //puzzlepieces[0].style.backgroundPosition = 0 + 'px' + ' ' + 0 + 'px';
+    //puzzlepieces[1].style.backgroundPosition = 100;
+    for (var i = 0; i < puzzlepieces.length; i++) 
+    {
+        puzzlepieces[i].style.backgroundPosition =  '-' + 300 + 'px' + ' ' + 0 + 'px';
+        //puzzlepieces[i].style.top = (parseInt(i / 4) * 100) + 'px';
+        //puzzlepieces[i].style.left = (i % 4 * 100) + 'px';
+        //puzzlepieces[i].style.backgroundPosition = '-' + puzzlepieces[i].style.left + ' ' + '-' + puzzlepieces[i].style.top;
+    }
+   }*/
+
+    /*function tileBackground() 
+    {
+        var puzzlepieces, x;
+
+        setInterval(function() {
+            for (var i = 0; i < puzzlepieces.length; i++) {
+                puzzlepieces[i].style.backgroundPosition = 
+            }
+
+        }, 500);
+   
+
+    }*/
+/*Extra Features*/
+
+//Multiple Background
+ function additionalFeature () 
+ {
+     var div = document.getElementById("picChoice");
+     var array = ["Smoking Joker", "Justice League", "Painted Joker", "Joker with Umbrella", "Butterfly"];
+     var selectList = document.createElement("select");
+     selectList.id = "picSelect";
+     div.appendChild(selectList);
+
+     for (var i = 0; i < array.length; i++) 
+     {
+         var option = document.createElement("option");
+         option.value = array[i];
+         option.text = array[i];
+         selectList.appendChild(option);
+     }
+ }
+
